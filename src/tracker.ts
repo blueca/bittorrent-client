9c0b2d357d64992555741e901225053ccd33887a
blueca <nickoldacre@outlook.com>
2020-08-15 13:55:44 +0100

    build functions for communicating with tracker

---
+++ src/tracker.ts
@@ -0,0 +1,117 @@
+import dgram from 'dgram';
+import { Buffer } from 'buffer';
+import { parse, UrlWithParsedQuery } from 'url';
+import crypto from 'crypto';
+import genId from './utils';
+import torrentParser from './torrent-parser';
+
+interface info {
+  files: string[];
+  length: number;
+}
+
+interface torrent {
+  info: info;
+  announce: any;
+}
+
+function getPeers(torrent: torrent, cb: CallableFunction): void {
+  const socket = dgram.createSocket('udp4');
+  const url: UrlWithParsedQuery = torrent.announce.toString('utf8');
+
+  udpSend(socket, buildConnReq(), url);
+
+  socket.on('message', (response) => {
+    if (respType(response) === 'connect') {
+      const connResp = parseConnResp(response);
+      const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
+
+      udpSend(socket, announceReq, url);
+    } else if (respType(response) === 'announce') {
+      const announceResp = parseAnnounceResp(response);
+
+      cb(announceResp.peers);
+    }
+  });
+}
+
+const udpSend = (
+  socket: dgram.Socket,
+  message: Buffer,
+  url: UrlWithParsedQuery,
+  cb = () => {
+    return null;
+  }
+) => {
+  const parsedUrl = parse(url.href);
+  if (!url.port || !url.host) throw 'url missing properties';
+  socket.send(message, 0, message.length, parseInt(url.port), url.host, cb);
+};
+
+const respType = (resp: Buffer): string => {
+  return 'test';
+};
+
+const buildConnReq = () => {
+  const buffer = Buffer.alloc(16);
+
+  buffer.writeUInt32BE(0x417, 0);
+  buffer.writeUInt32BE(0x271019890, 4);
+  buffer.writeUInt32BE(0, 8);
+  crypto.randomBytes(4).copy(buffer, 12);
+
+  return buffer;
+};
+
+const parseConnResp = (resp: Buffer) => {
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    connectionId: resp.slice(8)
+  };
+};
+
+const buildAnnounceReq = (connId: Buffer, torrent: torrent, port = 6881) => {
+  const buffer = Buffer.allocUnsafe(98);
+
+  connId.copy(buffer, 0);
+  buffer.writeUInt32BE(1, 8);
+  crypto.randomBytes(4).copy(buffer, 12);
+  torrentParser.infoHash(torrent).copy(buffer, 16);
+  genId().copy(buffer, 36);
+  Buffer.alloc(8).copy(buffer, 56);
+  torrentParser.size(torrent).copy(buffer, 64);
+  Buffer.alloc(8).copy(buffer, 72);
+  buffer.writeUInt32BE(0, 80);
+  buffer.writeUInt32BE(0, 84);
+  crypto.randomBytes(4).copy(buffer, 88);
+  buffer.writeInt32BE(-1, 92);
+  buffer.writeUInt16BE(port, 96);
+
+  return buffer;
+};
+
+const parseAnnounceResp = (resp: Buffer) => {
+  const group = (iterable: Buffer, groupSize: number) => {
+    const groups = [];
+    for (let i = 0, len = iterable.length; i < len; i += groupSize) {
+      groups.push(iterable.slice(i, i + groupSize));
+    }
+    return groups;
+  };
+
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    leechers: resp.readUInt32BE(8),
+    seeders: resp.readUInt32BE(12),
+    peers: group(resp.slice(20), 6).map((address) => {
+      return {
+        ip: address.slice(0, 4).join('.'),
+        port: address.readUInt16BE(4)
+      };
+    })
+  };
+};
+
+export default getPeers;
