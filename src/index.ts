0a0ea57dfb8a76be5648c93a8208067adec2643d
blueca <nickoldacre@outlook.com>
2020-08-15 13:56:34 +0100

    move initial logic into a run function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import fs from 'fs';
 import bencoder from 'bencoder';
 import getPeers from './tracker';
+import torrentParser from './torrent-parser';
 
-const torrent = fs.promises
-  .readFile('puppy.torrent')
-  .then((buffer) => {
-    const torrentDetails = bencoder.decode(buffer, 'utf8');
+const run = async () => {
+  const torrent = await torrentParser.open('puppy.torrent');
 
-    getPeers(torrentDetails, (peers: number[]) => {
-      console.log('>> peers: ', peers);
-    });
-  })
-  .catch((err) => {
-    throw err;
+  getPeers(torrent, (peers: number[]) => {
+    console.log('>> peers: ', peers);
   });
+};
+
+run();
+
+// const torrent = fs.promises
+//   .readFile('puppy.torrent')
+//   .then((buffer) => {
+//     const torrentDetails = bencoder.decode(buffer, 'utf8');
+
+//     getPeers(torrentDetails, (peers: number[]) => {
+//       console.log('>> peers: ', peers);
+//     });
+//   })
+//   .catch((err) => {
+//     throw err;
+//   });
