3c6f6e3b7a6bec5578af76f4e04b726d3b958d06
blueca <nickoldacre@outlook.com>
2020-08-25 17:00:52 +0100

    create Queue class to replace queue object, to allow for blocks and pieces

---
+++ src/Queue.ts
@@ -0,0 +1,40 @@
+import { torrentType, pieceBlockType } from './interfaces';
+import torrentParser from './torrent-parser';
+
+class Queue {
+  private _torrent: torrentType;
+  private _queue: pieceBlockType[];
+  private _choked: boolean;
+
+  constructor(torrent: torrentType) {
+    this._torrent = torrent;
+    this._queue = [];
+    this._choked = true;
+  }
+
+  queue(pieceIndex: number): void {
+    const nBlocks = torrentParser.blocksPerPiece(this._torrent, pieceIndex);
+    for (let i = 0; i < nBlocks; i++) {
+      const pieceBlock = {
+        index: pieceIndex,
+        begin: i * torrentParser.BLOCK_LEN,
+        length: torrentParser.blockLen(this._torrent, pieceIndex, i)
+      };
+      this._queue.push(pieceBlock);
+    }
+  }
+
+  deQueue(): pieceBlockType | undefined {
+    return this._queue.shift();
+  }
+
+  peek(): pieceBlockType {
+    return this._queue[0];
+  }
+
+  length(): number {
+    return this._queue.length;
+  }
+}
+
+export default Queue;
