3ad746d37948a84bd5a238e0ac0935431b0f9d05
blueca <nickoldacre@outlook.com>
2020-08-25 16:16:33 +0100

    create functions to determine piece/block length

--- src/torrent-parser.ts
+++ src/torrent-parser.ts
@@ -4,6 +4,8 @@ import crypto from 'crypto';
 import { Buffer } from 'buffer';
 import { torrentType } from './interfaces';
 
+const BLOCK_LEN: number = Math.pow(2, 14);
+
 const open = (filepath: string): Promise<torrentType> => {
   return fs.promises
     .readFile(filepath)
@@ -30,4 +32,39 @@ const infoHash = (torrent: torrentType): Buffer => {
   return crypto.createHash('sha1').update(info).digest();
 };
 
-export default { open, size, infoHash };
+const pieceLen = (torrent: torrentType, pieceIndex: number): number => {
+  const totalLen = parseInt(size(torrent).readBigInt64BE().toString());
+  const pieceLen = torrent.info['piece length'];
+  const lastPieceLen = totalLen % pieceLen;
+  const lastPieceIndex = Math.floor(totalLen / pieceLen);
+
+  return lastPieceIndex === pieceIndex ? lastPieceLen : pieceLen;
+};
+
+const blocksPerPiece = (torrent: torrentType, pieceIndex: number): number => {
+  const pieceLength = pieceLen(torrent, pieceIndex);
+
+  return Math.ceil(pieceLength / BLOCK_LEN);
+};
+
+const blockLen = (
+  torrent: torrentType,
+  pieceIndex: number,
+  blockIndex: number
+): number => {
+  const pieceLength = pieceLen(torrent, pieceIndex);
+  const lastPieceLength = pieceLength % BLOCK_LEN;
+  const lastPieceIndex = Math.floor(pieceLength / BLOCK_LEN);
+
+  return blockIndex === lastPieceIndex ? lastPieceLength : BLOCK_LEN;
+};
+
+export default {
+  open,
+  size,
+  infoHash,
+  BLOCK_LEN,
+  pieceLen,
+  blocksPerPiece,
+  blockLen
+};
