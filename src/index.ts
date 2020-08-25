d9aee0558cba522ffb2054950321653ce0c9ffea
blueca <nickoldacre@outlook.com>
2020-08-25 16:12:10 +0100

    disable no-console rule for catch block

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import torrentParser from './torrent-parser';
 import downloadTorrent from './download';
 
 const run = async () => {
-  const torrent = await torrentParser.open(process.argv[2]).catch((err) => {
-    console.log(err);
-  });
+  // eslint-disable-next-line no-console
+  const torrent = await torrentParser.open(process.argv[2]).catch(console.log);
 
   if (torrent) downloadTorrent(torrent);
 };
